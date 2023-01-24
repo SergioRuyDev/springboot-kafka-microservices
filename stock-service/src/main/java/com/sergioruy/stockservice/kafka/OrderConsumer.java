191c1d3f59de572d9ec170b81fcefdfbbaff1ad5
sergioruydev <sergioruyenator@gmail.com>
2023-01-24 17:11:47 -0300

    Start consumers config wip.

---
+++ stock-service/src/main/java/com/sergioruy/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,14 @@
+package com.sergioruy.stockservice.kafka;
+
+import com.sergioruy.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+//    public void consumer(OrderEvent);
+}
