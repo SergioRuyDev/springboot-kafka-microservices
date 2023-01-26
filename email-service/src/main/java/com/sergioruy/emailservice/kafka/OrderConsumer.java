8f46d98bf26108d930bd56541d70c109ef518fda
sergioruydev <sergioruyenator@gmail.com>
2023-01-26 14:38:01 -0300

    Email service done.

---
+++ email-service/src/main/java/com/sergioruy/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,21 @@
+package com.sergioruy.emailservice.kafka;
+
+import com.sergioruy.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event) {
+        LOGGER.info(String.format("Order event received in e-mail service => %s", event.toString()));
+
+        // send the e-mail to the customer
+
+    }
+}
